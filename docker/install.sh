c6695eb62b86c19e3ad912bef91481b544d79b2a
fjrmoreews <fjrmoreews@gmail.com>
2019-01-14 11:19:09 +0100

    -y

--- docker/install.sh
+++ docker/install.sh
@@ -6,13 +6,13 @@
 #echo "127.0.1.1 $HOSTNAME" > /etc/hosts
 
 sudo apt-get update
-sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
+sudo apt-get install    -y  apt-transport-https     ca-certificates     curl     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)    stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +rx /usr/local/bin/docker-compose
