ca0b48cf4a273b4317ea03599472efc919d4a3c0
fjrmoreews <fjrmoreews@gmail.com>
2019-01-23 11:11:52 +0100

    NFS mount for debian and ubuntu

---
+++ mount/nfs_client.sh
@@ -0,0 +1,38 @@
+#bin/bash
+############
+# NFS client mount
+# for debian + ubuntu
+############
+#can be used for openstack manila SHARES
+apt-get install -y nfs-common
+#########"edit this######
+NFS_SERVER_IP=xxxxx
+SPATH=$NFS_SERVER_IP:/var/lib/manila/mnt/share-xxxxxxxxxxxx
+SHAREDIR=/mnt/shares1
+#########################
+
+function simplemnt {
+
+ mkdir -p $SHAREDIR && chmod 744 $SHAREDIR
+ mount $SPATH   $SHAREDIR  -vvvv
+ 
+}
+
+function unmnt {
+ 
+  umount $SHAREDIR
+}
+
+#############fstab version
+
+function fstabmnt {
+
+ sudo chmod  777  /etc/fstab
+ OPTFSTAB=""
+ #for old ubuntu version (e.g ubuntu 14-> nfs version 3)
+ #OPTFSTAB=",nfsvers=3"
+ echo "$SPATH $SHAREDIR  nfs rw$OPTFSTAB 0 0" >>  /etc/fstab
+ chmod  644  /etc/fstab
+}
+
+fstabmnt
